8061763057fee9be9b1038137e0445694fe2e9ab
Thibaud <thibaud29@protonmail.com>
2018-09-29 16:26:24 +0200

    Category resource controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+
+    /**
+     * Get all Categories
+     *
+     * @return Category
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Store new Category
+     *
+     * @param Request $request
+     * @return Category $category
+     */
+    public function store(Request $request)
+    {
+        $category = Category::create([
+            'name' => $request->name,
+        ]);
+
+        return $category;
+    }
+
+    /**
+     * Get specific Category
+     *
+     * @param int $id
+     * @return Category $category
+     */
+    public function show($id)
+    {
+        try
+        {
+            $category = Category::findOrFail($id);
+            return $category;
+        } 
+        catch(\Exception $e) 
+        {
+            return response()->json([
+                'message' => 'error',
+                'description' => 'Unable to find this category ...'
+            ]);
+        }
+    }
+    
+    /**
+     * Update specific Category
+     *
+     * @param int $id
+     * @param Request $request
+     * @return Category $category
+     */
+    public function update($id, Request $request)
+    {
+        try
+        {
+            $category = Category::findOrFail($id);
+
+            $category->name = $request->name;
+            $category->save();
+            
+            return $category;
+        } 
+        catch(\Exception $e) 
+        {
+            return response()->json([
+                'message' => 'error',
+                'description' => 'Unable to find this category ...'
+            ]);
+        }
+    }
+
+    /**
+     * Delete specific Category
+     *
+     * @param int $id
+     * @return string
+     */
+    public function destroy($id)
+    {
+        try
+        {
+            $category = Category::findOrFail($id);
+            $category->delete();
+
+            return response()->json([
+                'message' => 'success'
+            ]);
+        } 
+        catch(\Exception $e) 
+        {
+            return response()->json([
+                'message' => 'error',
+                'description' => 'Unable to find this category ...'
+            ]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,17 @@ $router->get('/', 'HomeController@index'); // Show a simple homepage
 /    API routes
 /--------------------
 */
+
+// Appreciations routes
 $router->get('/api/appreciations', 'AppreciationController@index'); //All appreciations
 $router->post('/api/appreciations', 'AppreciationController@store'); //Create appreciation
 $router->get('/api/appreciations/{id}', 'AppreciationController@show'); //Show specific appreciation
 $router->put('/api/appreciations/{id}', 'AppreciationController@update'); //Update specific appreciation
 $router->delete('/api/appreciations/{id}', 'AppreciationController@destroy'); //Delete specific appreciation
+
+// Categories routes
+$router->get('/api/categories', 'CategoryController@index'); //All categories
+$router->post('/api/categories', 'CategoryController@store'); //Create category
+$router->get('/api/categories/{id}', 'CategoryController@show'); //Show specific category
+$router->put('/api/categories/{id}', 'CategoryController@update'); //Update specific category
+$router->delete('/api/categories/{id}', 'CategoryController@destroy'); //Delete specific category
